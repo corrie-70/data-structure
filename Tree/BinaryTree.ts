3e1c8ebf7426935e269529e9cb85d7cc5a22c713
huangke <huangke@skieer.cn>
2020-09-15 17:45:00 +0800

    二叉树数据结构

--- Tree/BinaryTree.ts
+++ Tree/BinaryTree.ts
@@ -14,26 +14,88 @@ export class BinaryTree {
     private head: BTNode | null = null;
 
     /**
+     * 销毁二叉树
+     */
+    destroyBT() {
+        this.head = null;
+    }
+
+    /**
+     * 查找元素值为ele的节点
+     * @param ele
+     */
+    findNode(ele: string): BTNode | null {
+        let p: BTNode;
+        if (this.head === null) {
+            return null;
+        }
+        if (this.head.data === ele) {
+            return this.head;
+        }
+        // 深度优先遍历，定义栈存储待查找变量
+        let stack = new SqStack<BTNode>();
+        stack.push(this.head);
+        while (!stack.stackEmpty()) {
+            const sTop = stack.getTop() as BTNode;
+            if (sTop.data === ele) {
+                return sTop;
+            }
+            p = stack.pop() as BTNode;
+            if (p.lChild) {
+                stack.push(p.lChild);
+            }
+            if (p.rChild) {
+                stack.push(p.rChild);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 返回二叉树高度
+     */
+    getBTNodeDepth(): number {
+        return this.getNodeDepth(this.head);
+    }
+
+    /**
+     * 返回二叉树括号表示法字符串
+     */
+    dispBTNode(): string {
+        if (this.head) {
+            return this.getBTNode(this.head);
+        }
+        return null;
+    }
+
+    /**
      * 创建二叉树
-     * @param str 
+     * @param str
      */
     private createBTNode(str: string) {
         let res: BTNode | null = null;
+        //栈保存节点
         const stack = new SqStack<BTNode>();
-        let k = 0, p: BTNode;
-        str.split('').forEach(item => {
+        //标记左右子树：1→左子树，2→右子树
+        let k = 0,
+            p: BTNode;
+        str.split("").forEach((item) => {
             switch (item) {
-                case '(':
+                case "(":
                     k = 1;
+                    //节点有子元素，将节点入栈
                     stack.push(p);
                     break;
-                case ')':
+                case ")":
+                    //节点子元素计算完成，出栈
                     stack.pop();
                     break;
-                case ',':
+                case ",":
                     k = 2;
                     break;
                 default:
+                    //计算当前节点值，并在链表中找到它所处的位置
+                    //即栈顶元素的子节点
                     p = new BTNode(item);
                     if (!res) {
                         res = p;
@@ -50,51 +112,45 @@ export class BinaryTree {
                     }
                     break;
             }
-        })
+        });
         this.head = res;
     }
 
     /**
-     * 销毁二叉树
+     * 递归计算元素值
+     * @param node
+     * @param data
      */
-    destroyBT() {
-
-    }
-
-    findNode(ele) {
-
-    }
-
-    lChildNode() {
-
-    }
-
-    rChildNode() {
-
-    }
-
-    BTNodeDepth() {
-
+    private getBTNode(node: BTNode): string {
+        let str = "";
+        if (node !== null) {
+            str += node.data;
+            if (node.lChild !== null || node.rChild !== null) {
+                str += "(";
+                str += this.getBTNode(node.lChild);
+                if (node.rChild !== null) {
+                    str += ",";
+                    str += this.getBTNode(node.rChild);
+                }
+                str += ")";
+            }
+        }
+        return str;
     }
 
     /**
-     * 返回二叉树括号表示法字符串
+     * 计算节点高度
+     * @param node
      */
-    dispBTNode(): string {
-        if (this.head) {
-            let str = this.head.data;
-            return str;
+    private getNodeDepth(node: BTNode): number {
+        let lDepth: number, rDepth: number;
+        if (node === null) {
+            return 0;
+        } else {
+            lDepth = this.getNodeDepth(node.lChild);
+            rDepth = this.getNodeDepth(node.rChild);
+            return lDepth > rDepth ? lDepth + 1 : rDepth + 1;
         }
-        return null
-    }
-
-    private getBTNodeData(node: BTNode): string {
-        if (!node) {
-            // if () {
-
-            // }
-        }
-        return null
     }
 }
 
@@ -115,4 +171,7 @@ export class BTNode {
 
 //test code
 const bTree = new BinaryTree("a(b(d(,g)),c(e,f))");
-console.log('bTree', bTree)
+// console.log("bTree", JSON.stringify(bTree));
+console.log("findNode", bTree.findNode("c"));
+console.log("dispBTNode", bTree.dispBTNode());
+console.log("getBTNodeDepth", bTree.getBTNodeDepth());
